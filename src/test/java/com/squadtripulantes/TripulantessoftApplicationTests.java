32c28832c972e0741d1212b977c73170326d9131
Kevin Santiago Narvaez Vera <knarvaez@excelcredit.co>
2022-08-24 21:39:22 -0500

    Primer commit, hasta este punto, el programa muestra en consola un Hola Mundo

---
+++ src/test/java/com/squadtripulantes/TripulantessoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squadtripulantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripulantessoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
