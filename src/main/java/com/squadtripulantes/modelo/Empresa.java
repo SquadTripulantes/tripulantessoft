2ef187998153b361fc3aab84e2a3ef4b739ce3aa
Sterly <sterlycalle@gmail.com>
2022-08-28 22:29:04 +0200

    se agrega clase Empresa

--- src/main/java/com/squadtripulantes/modelo/Empresa.java
+++ src/main/java/com/squadtripulantes/modelo/Empresa.java
@@ -1,4 +1,70 @@
 package com.squadtripulantes.modelo;
 
+import java.util.Date;
+
 public class Empresa {
+    public String id;
+    public String nombre;
+    public String telefono;
+    public Empleado usuario;
+    public Date creadoEn;
+    public Date modificadoEn;
+
+
+    public empresa(String id, String nombre, String telefono, Empleado usuario, Date creadoEn, Date modificadoEn) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.usuario = usuario;
+        this.creadoEn = creadoEn;
+        this.modificadoEn = modificadoEn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setInombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Empleado getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Empleado usuario) {
+        this.usuario = usuario;
+    }
+
+    public Date getCreadoEn() {
+        return creadoEn;
+    }
+
+    public void setCreadoEn(Date creadoEn) {
+        this.creadoEn = creadoEn;
+    }
+
+    public Date getModificadoEn() {
+        return modificadoEn;
+    }
+
+    public void setModificadoEn(Date modificadoEn) {
+        this.modificadoEn = modificadoEn;
+    }
 }
