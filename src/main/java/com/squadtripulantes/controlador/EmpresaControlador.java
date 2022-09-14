86898116ae89df57632e4be194555f8edb653094
Santiago <ReickGaming@gmail.com>
2022-09-13 21:51:43 -0500

    Controlador de Empresa terminado.

--- src/main/java/com/squadtripulantes/controlador/EmpresaControlador.java
+++ src/main/java/com/squadtripulantes/controlador/EmpresaControlador.java
@@ -1,5 +1,41 @@
 package com.squadtripulantes.controlador;
+import com.squadtripulantes.modelo.*;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+
+
+@RestController
 public class EmpresaControlador {
+
+    @GetMapping("/enterprise")
+    public Empresa getEmpresa()
+    {
+        return new Empresa(0, null, null, null, null,
+                null, null,null, null);
+    }
+    @PostMapping("/enterprise")
+    public Empresa crearEmpresa(@RequestBody Empresa empresa)
+    {
+        return empresa;
+    }
+
+    @GetMapping("/enterprise/{id}")
+    public Empresa consultarEmpresa(@PathVariable("id") long index)
+    {
+        return new Empresa();
+    }
+
+    @PatchMapping("/enterprise/{id}")
+    public Empresa modificarEmpresa(@PathVariable("id") long index,@RequestBody Empresa empresa)
+    {
+        return  empresa;
+    }
+    @DeleteMapping("/enterprise/{id}")
+    public boolean eliminarEmpresa(@PathVariable("id") long index)
+    {
+        return true;
+    }
+
 
 }
