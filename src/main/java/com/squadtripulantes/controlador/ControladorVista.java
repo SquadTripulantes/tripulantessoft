20cc01985f72468dce4c8e55c4e415820252604f
Kevin Santiago Narvaez Vera <knarvaez@excelcredit.co>
2022-09-27 20:32:37 -0500

    Se esta creando login y funciona

--- src/main/java/com/squadtripulantes/controlador/ControladorVista.java
+++ src/main/java/com/squadtripulantes/controlador/ControladorVista.java
@@ -3,6 +3,8 @@ package com.squadtripulantes.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,7 +23,7 @@ public class ControladorVista {
 	private MovimientoDineroServicio movimientoDineroServicio;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String paginaPrincipal() {		
+	public String paginaPrincipal(Model model, @AuthenticationPrincipal OidcUser principal) {
 		return "index";
 	}
 	
